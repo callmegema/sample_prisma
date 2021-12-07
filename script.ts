6b8b3b648e2af587fe04ad7dae449c44e9ca88ca
callmegema <863121+callmegema@users.noreply.github.com>
2021-12-07 21:58:04 +0300

    first commit

---
+++ script.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+// A `main` function so that you can use async/await
+async function main() {
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  })
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
